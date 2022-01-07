bb43e8887af33bd42f01282902c2114e78d1d472
Fanouu <74674330+Fanouu@users.noreply.github.com>
2022-01-07 14:42:33 +0100

    Update RedemTask.php

--- src/Solaria/Tasks/RedemTask.php
+++ src/Solaria/Tasks/RedemTask.php
@@ -24,7 +24,7 @@ class RedemTask extends Task {
         if ($this->time === 0) {
 
             Core::getInstance()->getServer()->shutdown();
-            Utils::sendWebHook("・**{$sender->getName()}** viens de redem le serveur.", "**REDEM**", "https://discord.com/api/webhooks/926017966221447189/evJdzQjz6SsGi7-ow0-I5_MdNX0sYw7od8hiCsRh_vjVTNX0HwQg90DfuOr0HGVwyyaD");
+            Utils::sendWebHook("・**{$sender->getName()}** viens de redem le serveur.", "**REDEM**", "drop_your_webhook_link");
             Core::getInstance()->getScheduler()->cancelTask($this->getTaskId());
 
         }else{
