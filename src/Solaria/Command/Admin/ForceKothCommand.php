1ef0d70eaf1a7c191bb9ded5db603184ceb2003c
Fanouu <74674330+Fanouu@users.noreply.github.com>
2022-01-07 14:36:53 +0100

    Update ForceKothCommand.php

--- src/Solaria/Command/Admin/ForceKothCommand.php
+++ src/Solaria/Command/Admin/ForceKothCommand.php
@@ -23,7 +23,7 @@ class ForceKothCommand extends Command{
         if(!$sender->hasPermission(Permissions::GIVEPB)) return $sender->sendMessage("Tu n'as pas la permission de faire ceci");
         $sender->sendMessage(Utils::getPrefix() . "Tu as bien forcé le démarage du koth");
         $this->plugin->getKothAPI()->start();
-        Utils::sendWebHook("Un événement **KOTH** vient d'être start par un **admin**, soyez le premier à capturer !", "**Event KOTH**", "https://discord.com/api/webhooks/926021554020712471/5koyodiBfFZRU9mSNdpdsoQEToB1exmUumAyVJNF7pPY4e8kDcu8td4zyIwflajI9iIZ");
+        Utils::sendWebHook("Un événement **KOTH** vient d'être start par un **admin**, soyez le premier à capturer !", "**Event KOTH**", "drop_your_webhook_link");
         return true;
     }
 }
